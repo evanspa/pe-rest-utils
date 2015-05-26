93e6a51573b2cf16c4b07df8bb5cbd28ac58dd6c
Paul Evans <evansp2@gmail.com>
2015-05-26 09:32:14 -0400

    Version 0.0.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pe-rest-utils "0.0.6"
+(defproject pe-rest-utils "0.0.7-SNAPSHOT"
   :description "A Clojure library providing a set of helper functions for building REST APIs on top of Datomic."
   :url "https://github.com/evanspa/pe-rest-utils"
   :license {:name "MIT"
